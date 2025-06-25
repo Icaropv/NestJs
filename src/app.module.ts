7ff8f417e54a85dc84cf2c65321e3529b9c9eb2b
icaropv <icaropaiva0112@gmail.com>
2025-06-25 08:30:28 -0300

    feat: criando modulos de auth e user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
+  providers: [AuthService],
 })
 export class AppModule {}
